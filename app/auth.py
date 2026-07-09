791db18cf6881b7aec77dd1615a98e4087e78b25
Keshav <tingobingo5@gmail.com>
2026-07-09 21:40:05 +0530

    OAuth2 form login, Authorize flow, protected /me verified

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,14 @@
+from dns.dnssec import algorithm_to_text
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
 from jose import jwt, JWTError
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, oauth2
+from sqlalchemy.orm import Session
+from .database import get_db
+from . import models
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -22,4 +30,27 @@ def create_access_token(user_id : int) -> str:
 
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
+#who is making the request is answered by the vlock below
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) :
+    credentials_error = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms = [ALGORITHM])
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_error
+    except JWTError:
+        raise credentials_error
+
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise credentials_error
+    return user
 
--- app/routers/users.py
+++ app/routers/users.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from .. import models, schemas
 from ..database import get_db
 from ..auth import hash_password, verify_password, create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -23,11 +24,17 @@ def signup(user_in: schemas.UserCreate, db: Session = Depends(get_db)):
     return user
 
 @router.post("/login", response_model=schemas.TokenOut)
-def login(credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == credentials.email).first()
-    if not user or not verify_password(credentials.password, user.password_hash):
+def login(form_data: OAuth2PasswordRequestForm=Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.password_hash):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect email or password")
     return {"access_token": create_access_token(user.id)}
+
+from ..auth import get_current_user
+
+@router.get("/me", response_model=schemas.UserOut)
+def read_me(current_user: models.User = Depends(get_current_user)):
+    return current_user
 
 
 
